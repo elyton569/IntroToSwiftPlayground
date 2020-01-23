c57659e4a77abc9edabf5663adf8c5ac4acbb712
CM Student <cmstudent@UHWO-ACM-10.uhwo.hawaii.edu>
2020-01-23 10:45:23 -1000

    Finished Swift PlayGrounds Assignment #1

--- SwiftPlayground 1.playground/Contents.swift
+++ SwiftPlayground 1.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//var myFirstVariable: String
+var myFirstVariable = "Ive created my first varaible"
+
+//We're going to do something with strings
+var mySecondVariable = "and look I added two strings together"
+
+
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+print (myThirdVariable)
+
+// Assignment #2 Introduction to True and False
+
+var appState = false
+print(appState)
+
+//Part 5
+
+var numberOne = 5
+var numberTwo = 7
+
+var comparison = numberOne < numberTwo
+
+var numberThree = 4
+var numberFour = 6
+
+var difference = numberFour > numberThree
